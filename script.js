d4533893fc316313faa54aade7182963d0db0b0e
philward_io <54246354+phil-ward@users.noreply.github.com>
2026-02-28 22:52:39 +0100

    Create operate function to perform math operations

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2
 }
+
+let numberOne
+let numberTwo
+let operator
+
+function operate(num1, num2, operation){
+    switch (operation){
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            return divide(num1, num2)
+    }
+}
