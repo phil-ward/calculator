61bd01680d01eb8e631a3f1135ffb6e298702299
philward_io <54246354+phil-ward@users.noreply.github.com>
2026-03-07 10:59:41 +0100

    Add Button Input Functionality
    Create function to update output to screen
    Create event listeners for each Button
    Link = button to previously created operate function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const output = document.querySelector("div.output")
+
+const operators = ["+", "-", "*", "/"]
 function add(num1,num2){
     return num1 + num2
 }
@@ -14,9 +17,9 @@ function divide(num1, num2){
     return num1 / num2
 }
 
-let numberOne
-let numberTwo
-let operator
+let numberOne = ""
+let numberTwo = ""
+let operator = ""
 
 function operate(num1, num2, operation){
     switch (operation){
@@ -30,3 +33,34 @@ function operate(num1, num2, operation){
             return divide(num1, num2)
     }
 }
+
+function updateScreen(){
+    output.innerText = `${numberOne} ${operator} ${numberTwo}`
+}
+
+
+
+const buttons = document.querySelectorAll("button")
+
+const clickEvent = (event) => {
+    if (event.target.innerText === '='){
+        let result = operate(parseInt(numberOne), parseInt(numberTwo), operator)
+        numberOne = result
+        operator = ""
+        numberTwo = ""
+        updateScreen
+    }
+    else if (operators.includes(event.target.innerText)){
+        operator = event.target.innerText
+    }else if (operator === "") {
+        numberOne += event.target.innerText
+    } else if (operator !== "") {
+        numberTwo += event.target.innerText
+    }
+    updateScreen()
+    console.log(`${event.target.innerText} button clicked!`)
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', clickEvent)
+})
